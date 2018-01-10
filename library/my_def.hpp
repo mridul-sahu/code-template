b2c9956c56c979e9660c201532973cac565840ba
Mridul <mridulsahu01@gmail.com>
2018-01-10 16:19:38 +0530

    My def updated

--- library/my_def.hpp
+++ library/my_def.hpp
@@ -7,6 +7,8 @@
 #define ull unsigned long long
 #define MOD (long long) (1e9 + 7)
 #define loop(i, a, n) for(ll i = (a); i < (n); ++i)
+#define rloop(i, a, n) for(ll i = ((n)-1); i >= (a); --i)
+#define LLINF numeric_limits<ll>::max()
 #define vl vector<ll>
 #define all(x) (x).begin(),(x).end()
 
